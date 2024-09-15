1d5d526751e4769c458081f8c34f081980265a86
Joy Rana <joyrana@Joys-MacBook-Pro.local>
2024-09-16 01:17:42 +0530

    intial commit 3

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,7 +1,7 @@
 const githubUsername = "joyrana";
 const mediumUsername = "joy-rana";
 
-const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
+const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${username}`;
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
 
 export const URLs = {
