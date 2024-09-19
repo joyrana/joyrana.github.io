71d67fed78da437052d4c7f566a50f523895dc49
Joy Rana <joyrana@Joys-MacBook-Pro.local>
2024-09-19 20:47:44 +0530

    fixed about

--- joyrana.github.io/data.js
+++ joyrana.github.io/data.js
@@ -1,5 +1,5 @@
 export const bio = [
-  "My name is Joy Rana. I'm a programmer,teacher, and a technologist. I work for Citi as an Assistant Vice President, Technology. I work on JavaScript, Open Source, and Artificial Intelligence, but posts on this portal are my own. I do blog about technology, gadgets, code, the web, where we’re going. I'm excited about machine learning, artificial intelligence and the open web.",
+  "My name is Joy Rana. I'm a programmer and a teacher. I work on JavaScript, Open Source, and Artificial Intelligence, but posts on this portal are my own. I do blog about technology, gadgets, code, the web, where we’re going. I'm excited about machine learning, artificial intelligence and the open web.",
 ];
 
 export const skills = [
